9234b6b76720e8d8d78a1ca4e4d86aff7715597b
Hernany Motoso <hernanymotoso@gmail.com>
2025-07-16 16:51:00 -0300

    style: make Header component responsive

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -14,17 +14,26 @@
 
 <header class="w-full bg-gray-800 shadow">
   <Wrapper>
-    <div class="mx-auto flex items-center justify-between">
-      <button class="flex cursor-pointer items-center space-x-2" on:click={() => goto('/', { invalidateAll: true })}>
-        <img src="/favicon.svg" alt="Logo" class="h-8 w-8" />
-        <span class="text-xl font-bold text-white">Book Manager</span>
-      </button>
+    <div class="mx-auto flex flex-col gap-2 px-2 py-2 sm:flex-row sm:items-center sm:justify-between sm:gap-0">
+      <div class="mb-6 flex w-full items-center justify-center space-x-2 sm:mb-0 sm:w-auto sm:justify-start">
+        <button
+          class="flex cursor-pointer items-center space-x-2"
+          on:click={() => goto('/', { invalidateAll: true })}
+          type="button"
+        >
+          <img src="/favicon.svg" alt="Logo" class="h-8 w-8" />
 
-      <div class="flex items-center space-x-4">
+          <span class="text-xl font-bold text-white">Book Manager</span>
+        </button>
+      </div>
+
+      <div class="flex flex-col items-center gap-2 sm:flex-row sm:gap-4">
         {#if user}
-          <a href="/profile" class="mr-2 text-blue-300 hover:underline" title="See profile">{user.email}</a>
+          <div class="flex flex-col-reverse items-center gap-2 sm:flex-row sm:gap-4">
+            <a href="/profile" class="text-blue-300 hover:underline" title="See profile">{user.email}</a>
 
-          <span class="text-gray-200">Hello, {user.name}</span>
+            <span class="text-gray-200">Hello, {user.name}</span>
+          </div>
 
           <button
             class="cursor-pointer rounded bg-red-500 px-4 py-2 text-white transition hover:bg-red-600"
